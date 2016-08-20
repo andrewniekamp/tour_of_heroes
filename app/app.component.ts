c812ca7135c6652414bea0239d1696425affc272
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-20 11:08:44 -0700

    add template area for list of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,12 @@ export class Hero {
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul>
+      <li>
+        <!-- each hero goes here -->
+      </li>
+    </ul>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -17,8 +23,22 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narce' },
+  { id: 13, name: 'Bobmasta' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Rubberman' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
